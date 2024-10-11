3bcb95632f067c6d6a32b5d91e8c447655ffa10c
Mostafaa117 <mostafa.hamed1173@gmail.com>
2024-10-11 21:14:49 +0300

    Upload Files

---
+++ main.js
@@ -0,0 +1,7 @@
+
+window.setTimeout(function popup(){
+document.getElementById("cont").style.display = "block";
+},5000) 
+function popclose(){
+  document.getElementById("cont").style.display = "none";
+}
