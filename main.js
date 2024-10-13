c367d6ab16347c2ea13856a7ecfa53134fa69024
Mostafaa117 <mostafa.hamed1173@gmail.com>
2024-10-13 21:05:42 +0300

    Delete Unnecessary Files

--- main.js
+++
@@ -1,7 +0,0 @@
-
-window.setTimeout(function popup(){
-document.getElementById("cont").style.display = "block";
-},5000) 
-function popclose(){
-  document.getElementById("cont").style.display = "none";
-}
